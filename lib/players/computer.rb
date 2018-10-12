f788e4c721e3e1e05de3b363c5b31db273a289d6
Heather Miller <heather.miller114@gmail.com>
2018-10-12 00:21:26 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,9 @@ module Players
     
     MOVE = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     def move(board)
-      if board.cells[0] = " "
+      if board.cells[0] == " "
+        "1"
+      elsif board.cells[]
     end
     
   end
